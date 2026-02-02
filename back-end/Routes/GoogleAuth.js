39120dd31243a4a50a20a6cf118d75cc5fe6a732
Aravind jinna <aravindjinna1@gmail.com>
2026-02-02 11:46:47 +0530

    callback url changed

--- back-end/Routes/GoogleAuth.js
+++ back-end/Routes/GoogleAuth.js
@@ -12,7 +12,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      // "/api/google/callback"
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
--- back-end/config/passport.js
+++ back-end/config/passport.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:process.env.GOOGLE_CALLBACKURL,
+      callbackURL:process.env.GOOGLE_CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({ email: profile.emails[0].value });
